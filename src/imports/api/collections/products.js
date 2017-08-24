fa8d329f368761e38d364d272cc759f0fda85071
Itamar Abeliovich <iabeliovich@gmail.com>
2017-08-24 23:58:44 +0300

    Finalized collections for now

--- src/imports/api/collections/products.js
+++ src/imports/api/collections/products.js
@@ -1,3 +1,19 @@
 /**
  * Created by itamar on 24/08/2017.
  */
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+export default Products = new Mongo.Collection('products');
+
+let schema = {};
+
+schema.Product = new SimpleSchema({
+    name: String,
+    imageLink: String,
+    price: {
+        type: Number,
+        min: 0,
+        exclusiveMin: true,
+    },
+});
---
+++ src/imports/api/methods/orders.js
@@ -0,0 +1,9 @@
+/**
+ * Created by itamar on 24/08/2017.
+ */
+import Orders from '/imports/api/collections/orders';
+import Meteor from 'meteor/meteor';
+
+Meteor.methods({
+
+});
