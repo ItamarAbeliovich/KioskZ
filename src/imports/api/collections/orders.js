1fb41dff4e54b32af442af24c3450654017bfc12
Itamar Abeliovich <iabeliovich@gmail.com>
2017-08-24 12:56:50 +0300

    Added orders schema

--- src/imports/api/collections/orders.js
+++ src/imports/api/collections/orders.js
@@ -5,3 +5,22 @@ import { Mongo } from 'meteor/mongo';
 import SimpleSchema from 'simpl-schema';
 
 export default Orders = new Mongo.Collection('orders');
+
+let schema = {};
+
+schema.OrderItem = new SimpleSchema({
+    quantity: {
+        type: Number,
+        min: 1,
+        // TODO: Think - do we want max?
+    },
+
+    product: SimpleSchema.RegEx.Id
+});
+
+schema.Order = new SimpleSchema({
+    createdAt: Date,
+    products: [schema.OrderItem],
+});
+
+Orders.attachSchema(schema.Order);
