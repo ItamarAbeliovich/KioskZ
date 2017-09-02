9e23ba2288aaf13fcc5dc40a048c58e815daf2ff
Itamar Abeliovich <iabeliovich@gmail.com>
2017-09-02 16:09:26 +0300

    Collections and methods are now imported on the client

--- src/imports/api/methods/index.js
+++ src/imports/api/methods/index.js
@@ -1,5 +1,5 @@
 /**
  * Created by itamar on 02/09/2017.
  */
-import 'orders';
-import 'products';
+import './orders';
+import './products';
--- src/imports/startup/server/register-api.js
+++ src/imports/startup/server/register-api.js
@@ -1,5 +1,3 @@
 // Register your apis here
 
 import '/imports/api/publications/index';
-import '/imports/api/collections/index';
-import '/imports/api/methods/index';
